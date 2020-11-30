a3ecc6ee5108250a38c00154617e0f4ce1d1e3ec
林駿綦 <r10101720307@gmail.com>
2020-12-01 00:20:39 +0800

    練習 8-1：修改 PostsController 的 index 方法，修改內容為 - 使用 Post.php（Model）查詢精選文章

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::where('is_feature', '=', 1)->orderBy('id', 'DESC')->get();
+        $data = [
+            'posts' => $posts,
+        ];
+        return view('posts.index', $data);
     }
 
     public function show($id)
